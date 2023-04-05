766eab3fca1c179c550d2c56be4661e618cf5e3d
Nog <63429016+GonzaloNog@users.noreply.github.com>
2023-04-05 19:59:54 -0300

    Fix

--- Ejercicios/Trabajo1.py
+++ Ejercicios/Trabajo1.py
@@ -1,18 +1,42 @@
 '''
 Un programa que te deje cargar usuarios en un sistema
 poder ver esos usuarios y que el programa termine con un comando de texto
+
+Condiciones de registro
+    - No tener espacios el nombre
+    - tener 6 o mas caracteres
+    - Comprobar que el usuario no este registrado 
+    Clave
+        - minimo 4 caracteres
 '''
 
 BD = {'Gonzalo','123456'}
 
 def Registro():
+    
     print('registro')
 
+
 def MostrarUsuarios():
-    print('Usuarios')
+    print('BD')
+
 
 def Main():
-    while(True):
-        break
+    game = True
+    while(game):
+        accion = input("Que quiere hacer escriba 'Comandos' para ver comandos): ")
+        if accion == "registros":
+            Registro()
+
+        elif accion == "mostrarUsuarios":
+            MostrarUsuarios()
+        
+        elif accion == "quit":
+            game = False
+
+        elif accion == "comandos":
+            print()
+        else:
+            print('Comando invalido')
 
 Main()
